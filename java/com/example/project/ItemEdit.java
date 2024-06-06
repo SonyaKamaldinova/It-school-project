ac806e32ce3a0b6cca283d4528b777d0c260ed3c
SonyaKamaldinova <149136073+SonyaKamaldinova@users.noreply.github.com>
2024-06-06 23:31:07 +0300

    Update ItemEdit.java

--- java/com/example/project/ItemEdit.java
+++ java/com/example/project/ItemEdit.java
@@ -10,34 +10,33 @@ import android.widget.EditText;
 public class ItemEdit extends Activity {
 	private Long nId;
 	private EditText nText;
+	
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.item_edit);
         setTitle(R.string.item_title);
         Button confirmButton = (Button) findViewById(R.id.add);
 		nText = (EditText) findViewById(R.id.body);
-		nId = (savedInstanceState == null) ? null :
-            (Long) savedInstanceState.getSerializable(DBAdapter.PRODUCTID);
+		nId = (savedInstanceState == null) ? null : (Long) savedInstanceState.getSerializable(DBAdapter.PRODUCTID);
 		//because use this activity for edit and make item
 		if (nId == null) {
 			Bundle extras = getIntent().getExtras();
-			nId = extras != null ? (extras.getLong(DBAdapter.PRODUCTID)>0?extras.getLong(DBAdapter.PRODUCTID):null) : null;}
-		if(nId != null){
+			nId = extras != null ? (extras.getLong(DBAdapter.PRODUCTID)>0 ? extras.getLong(DBAdapter.PRODUCTID) : null) : null;}
+		if(nId != null) {
 			DBAdapter db=new DBAdapter(this);
 			db.open();
 			Cursor c =db.getItem(nId) ;
 			nText.setText(c.getString(c.getColumnIndexOrThrow(DBAdapter.TITLE)));
-		    db.close();
-		}
-        confirmButton.setOnClickListener(new View.OnClickListener() {
+		    db.close();}
+		confirmButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
-            	DBAdapter db = new DBAdapter(view.getContext());
-            	db.open(); 
-            	if (nId == null) {
-            	    db.insertNewItem(nText.getText().toString(), getIntent().getExtras().getLong(DBAdapter.LIST_ID));}
+				DBAdapter db = new DBAdapter(view.getContext());
+				db.open(); 
+				if (nId == null) {
+					db.insertNewItem(nText.getText().toString(), getIntent().getExtras().getLong(DBAdapter.LIST_ID));}
 				else {
 					db.updateItem(nId, nText.getText().toString());}
-            	db.close();
-                setResult(RESULT_OK);
-                finish();}});}}
+				db.close();
+				setResult(RESULT_OK);
+				finish();}});}}
